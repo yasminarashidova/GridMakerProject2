1f65e64128bf60e7ae81fb36c9c59bc891bc114a
yasminarashidova <117114161+yasminarashidova@users.noreply.github.com>
2025-03-24 19:52:10 -0400

    Adding code to fillAll()

--- script.js
+++ script.js
@@ -40,6 +40,9 @@ function fillAll(){
 }
 
 // Clear all cells
-function clearAll(){
-    alert("Clicked Clear All"); // Replace this line with your code.
+function fillAll() {
+    const cells = document.getElementsByTagName("td");
+    for (let cell of cells) {
+        cell.style.backgroundColor = colorSelected;
+    }
 }
